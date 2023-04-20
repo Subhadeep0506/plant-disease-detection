682e3c6124783ca99fe764d8046352869e0df73a
Subhadeep Mandal <subhadeepdoublecap@gmail.com>
2023-04-20 18:21:37 +0530

    Minor changes

--- app.py
+++ app.py
@@ -68,13 +68,14 @@ def upload():
 
         pred_class = preds.argmax()
         
-        CATEGORIES = ['Pepper__bell___Bacterial_spot', 'Pepper__bell___healthy',
-                      'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy',
-                      'Tomato_Bacterial_spot', 'Tomato_Early_blight', 'Tomato_Late_blight',
-                      'Tomato_Leaf_Mold', 'Tomato_Septoria_leaf_spot',
-                      'Tomato_Spider_mites_Two_spotted_spider_mite', 'Tomato__Target_Spot',
-                      'Tomato__YellowLeaf__Curl_Virus', 'Tomato_mosaic_virus',
-                      'Tomato_healthy']
+        CATEGORIES = [
+            'Pepper__bell___Bacterial_spot', 'Pepper__bell___healthy',
+            'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy',
+            'Tomato_Bacterial_spot', 'Tomato_Early_blight', 'Tomato_Late_blight',
+            'Tomato_Leaf_Mold', 'Tomato_Septoria_leaf_spot',
+            'Tomato_Spider_mites_Two_spotted_spider_mite', 'Tomato__Target_Spot',
+            'Tomato__YellowLeaf__Curl_Virus', 'Tomato_mosaic_virus',
+            'Tomato_healthy']
         return diseases_dict[CATEGORIES[pred_class]]
 
     return None

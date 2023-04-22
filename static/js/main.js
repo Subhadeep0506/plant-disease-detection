b94213c1ef4ba5cba5d8550651756ffdd45d9d40
Subhadeep Mandal <subhadeepdoublecap@gmail.com>
2023-04-22 19:27:27 +0530

    Fixed deprecated script calls

--- static/js/main.js
+++ static/js/main.js
@@ -1,49 +1,54 @@
 $(document).ready(function () {
-  $(".image-section").hide();
-  $(".loader").hide();
-  $("#result").hide();
+  // Init
+  $('.image-section').hide();
+  $('.loader').hide();
+  $('#result').hide();
 
+  // Upload Preview
   function readURL(input) {
-    if (input.files && input.files[0]) {
-      var reader = new FileReader();
-      reader.onload = function (e) {
-        $("#imagePreview").css(
-          "background-image",
-          "url(" + e.target.result + ")"
-        );
-        $("#imagePreview").hide();
-        $("#imagePreview").fadeIn(650);
-      };
-      reader.readAsDataURL(input.files[0]);
-    }
+      if (input.files && input.files[0]) {
+          var reader = new FileReader();
+          reader.onload = function (e) {
+              $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
+              $('#imagePreview').hide();
+              $('#imagePreview').fadeIn(650);
+          }
+          reader.readAsDataURL(input.files[0]);
+      }
   }
   $("#imageUpload").change(function () {
-    $(".image-section").show();
-    $("#btn-predict").show();
-    $("#result").text("");
-    $("#result").hide();
-    readURL(this);
+      $('.image-section').show();
+      $('#btn-predict').show();
+      $('#result').text('');
+      $('#result').hide();
+      readURL(this);
   });
 
-  $("#btn-predict").click(function () {
-    var form_data = new FormData($("#upload-file")[0]);
+  // Predict
+  $('#btn-predict').click(function () {
+      var form_data = new FormData($('#upload-file')[0]);
 
-    $(this).hide();
-    $(".loader").show();
+      // Show loading animation
+      $(this).hide();
+      $('.loader').show();
 
-    $.ajax({
-      type: "POST",
-      url: "/predict",
-      data: form_data,
-      contentType: false,
-      cache: false,
-      processData: false,
-      async: true,
-      success: function (data) {
-        $(".loader").hide();
-        $("#result").fadeIn(600);
-        $("#result").text(" Prediction:  " + data);
-      },
-    });
+      // Make prediction by calling api /predict
+      $.ajax({
+          type: 'POST',
+          url: '/predict',
+          data: form_data,
+          contentType: false,
+          cache: false,
+          processData: false,
+          async: true,
+          success: function (data) {
+              // Get and display the result
+              $('.loader').hide();
+              $('#result').fadeIn(600);
+              $('#result').text(' Result:  ' + data);
+              console.log('Success!');
+          },
+      });
   });
+
 });
